2628d86ca4d2dd626a2b8e8b5c01f4c5dd6994da
davyken <davykennang552@gmail.com>
2025-01-24 12:50:27 +0100

    modiifed content

--- src/Pages/AuthPage/Registrations.jsx
+++ src/Pages/AuthPage/Registrations.jsx
@@ -148,7 +148,7 @@ const Registration = () => {
                                         {...register("password", { required: "Password is required" })}
                                         type={passwordVisible ? "text" : "password"}
                                         className="appearance-none block w-full pl-10 pr-12 px-3 py-2 border border-gray-700 rounded-lg bg-gray-800/50 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent transition-all"
-                                        placeholder="••••••••"
+                                        placeholder="Capital letter, min-5-words"
                                     />
                                     <button
                                         type="button"
